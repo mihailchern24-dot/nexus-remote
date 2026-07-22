d1d864f37fa09a3c959994d38ba2317935c5b3d1
mihailchern24-dot <mihailchern24@gmail.com>
2026-07-22 10:33:54 +0300

    Add HEAD handler for Render health check

--- health_server.py
+++ health_server.py
@@ -6,6 +6,9 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'OK')
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
 
 port = int(os.environ.get('PORT', 10000))
 HTTPServer(('0.0.0.0', port), HealthHandler).serve_forever()
